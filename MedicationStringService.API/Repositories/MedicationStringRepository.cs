de93e7c50e6083cd7d5e1e7c35beff9f0615b36b
Yeongjun Im <yeongjundev@gmail.com>
2020-03-11 00:57:39 +1100

    Comments added.

--- MedicationStringService.API/Repositories/MedicationStringRepository.cs
+++ MedicationStringService.API/Repositories/MedicationStringRepository.cs
@@ -12,16 +12,19 @@ namespace MedicationStringService.API.Repositories
     {
         public MedicationStringRepository(DbSet<MedicationString> dbSet) : base(dbSet) { }
 
+        // Get the total number of all MedicationStrings.
         public async Task<int> TotalCount()
         {
             return await context.CountAsync();
         }
 
+        // Get the sum of DosageCounts for all MedicationStrings.
         public async Task<int> TotalDosageCount()
         {
             return await context.SumAsync(medicationString => medicationString.DosageCount);
         }
 
+        // Get the number of MedicationStrings per each BottleSize.
         public async Task<IEnumerable<CountByBottleSize>> TotalNumberByBottleSize()
         {
             IEnumerable<CountByBottleSize> result = await context
@@ -34,6 +37,7 @@ namespace MedicationStringService.API.Repositories
             return result;
         }
 
+        // Get the number of MedicationStrings per each MedicationId.
         public async Task<IEnumerable<CountByMedicationId>> DistinctMedicationIds()
         {
             IEnumerable<CountByMedicationId> result = await context
